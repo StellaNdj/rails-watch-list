f01e38d6d7a5d4cf9f03f6978001359fb791953b
Stella Ndjikessi <stelladiana@live.fr>
2023-11-16 13:06:45 +0100

    List controller mostly done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :movies, only: [:index, :new, :create, :show]
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :destroy]
+  end
 end
